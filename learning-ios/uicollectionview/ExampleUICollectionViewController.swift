51587d7c6a04d7c925781213e696e8e0b14e3443
keijumt <keijumt@gmail.com>
2019-11-26 11:47:16 +0900

    Update uicollectionview even spacement

--- learning-ios/uicollectionview/ExampleUICollectionViewController.swift
+++ learning-ios/uicollectionview/ExampleUICollectionViewController.swift
@@ -9,13 +9,29 @@
 import UIKit
 
 class ExampleUICollectionViewController: UIViewController {
-
+    
     @IBOutlet weak var uiCollectionView: UICollectionView!
-
+    private let column: CGFloat = 3
+    private let sectionInset = UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.uiCollectionView.dataSource = self
+        let layout = UICollectionViewFlowLayout()
+        
+        // 周りの余白を設定
+        layout.sectionInset = self.sectionInset
+        
+        // アイテムの大きさを設定
+        let itemSpacing = self.sectionInset.left * (column - 1)
+        let availableWidth = self.view.bounds.width - itemSpacing - (sectionInset.left * 2)
+        layout.itemSize = CGSize(width: availableWidth / column, height: availableWidth / column)
+
+        // アイテムの垂直方向のスペースを設定
+        layout.minimumLineSpacing = sectionInset.left
+        
+        self.uiCollectionView.collectionViewLayout = layout
     }
 }
 
